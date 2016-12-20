059ae071d8e3b20a0508571950d7fca91fb5911f
martinsuba <martin.suba01@gmail.com>
2016-12-20 16:56:08 +0100

    initial commit

---
+++ center-image.js
@@ -0,0 +1,35 @@
+(function($) {
+
+	$.fn.centerImage = function(parentString) {
+		var _this = this;
+		
+		$(window).on('resize', function() {
+
+			_this.each(function() {
+				var $image = $(this),
+						$parent;
+
+				if (parentString)
+					$parent = $image.closest(parentString);
+				else
+					$parent = $image.parent();
+				
+				var _image = new Image();
+				_image.src = $image.attr("src");
+
+				var imageW  = _image.width,
+						imageH  = _image.height,
+						imageR  = imageW / imageH,
+						parentW = $parent.width(),
+						parentH = $parent.height(),
+						parentR = parentW / parentH;
+
+				if ( imageR > parentR )
+					$image.css({ width: 'auto', height: '100%' });
+				else
+					$image.css({ width: '100%', height: 'auto' });
+			});
+		}).trigger('resize');
+	};
+
+}(jQuery));
